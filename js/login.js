22ec133c6df1dc8850b305a8cc53f9c1079017f3
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-10-29 00:55:44 +0600

    design bank page,deposit and withdraw

--- js/login.js
+++ js/login.js
@@ -1,17 +1,16 @@
-document.getElementById('btn-submit').addEventListener('click',function(){
-    // console.log('submit clicked')
-    const emailField = document.getElementById('user-email');
-    const email = emailField.value;
-    console.log(email)
+document.getElementById("btn-submit").addEventListener("click", function () {
+  // console.log('submit clicked')
+  const emailField = document.getElementById("user-email");
+  const email = emailField.value;
+  console.log(email);
 
-    const passwordField = document.getElementById('user-password');
-    const password = passwordField.value;
-    console.log(password);
+  const passwordField = document.getElementById("user-password");
+  const password = passwordField.value;
+  console.log(password);
 
-    if(email === 'sakifhridoy@gmail.com' && password === 'secret'){
-        console.log('valid user')
-    }
-    else{
-        console.log('invalid user')
-    }
-})
+  if (email === "sakifhridoy@gmail.com" && password === "secret") {
+    window.location.href = "bank.html";
+  } else {
+    console.log("invalid user");
+  }
+});
