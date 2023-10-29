52bdfbc2d4c01f944993d8f37ddc81babe6caf0e
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-10-29 11:29:57 +0600

    balance fix

--- js/deposit.js
+++ js/deposit.js
@@ -1,6 +1,7 @@
 document.getElementById("btn-deposit").addEventListener("click", function () {
   // console.log('deposit clicked')
 
+//   deposit operation
   const depositField = document.getElementById("deposit-field");
   const newDepositAmountString = depositField.value;
 //   console.log(typeof newDepositAmount);
@@ -14,5 +15,19 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   depositTotalElement.innerText = currentDepositTotal;
 
 
-  depositField.value = "";
+
+//   Balance Total
+  const balanceTotalElement = document.getElementById('balance-total');
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+  balanceTotalElement.innerText = currentBalanceTotal;
+
+
+
+
+
+depositField.value = "";
+
 });
