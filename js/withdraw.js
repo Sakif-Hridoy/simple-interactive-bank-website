c1b306846f44014b80ab9b6d8190269639c68eea
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-10-29 14:06:34 +0600

    fix issues

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,21 +3,37 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    withdrawField.value = "";
+
+    if(isNaN(newWithdrawAmount)){
+        alert('Please provide a valid Number');
+        return;
+    }
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+    // if er niche dite hobe karon age update hoye jache
+    // const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    // withdrawTotalElement.innerText = currentWithdrawTotal;
 
-    withdrawField.value = "";
+    
 
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('Not Enough Balance to Withdraw');
+        return;
+    }
+
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal
     // withdrawTotalElement.innerText = newWithdrawAmount;
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
     
 })
